7b1ad01722272caf9616c301a8afea3570035a17
Strangexz <ja_figueroa@outlook.es>
2024-06-18 18:26:26 -0600

    feat(): nuevo controlador getOperation

--- src/api/controllers/basic.controller.js
+++ src/api/controllers/basic.controller.js
@@ -1,4 +1,10 @@
+/**
+ * Basic Controller
+ *
+ * @description :: Server-side controller for handling incoming requests.
+ */
 const _ = require('underscore');
+const validator = require('validator');
 const path = require('path');
 const { StatusCodes } = require('http-status-codes');
 
@@ -22,15 +28,93 @@ const getOperations = (req, res) => {
   try {
     console.log('Entrando al controlador getOperations...');
 
+    const methods = [];
     const response = {};
-    const result = BasicService.getOperations();
 
-    response['result'] = result.data;
-    response['isOk'] = result.isOk;
+    response['isOk'] = false;
+
+    if (!_.isUndefined(req.query.methods)) {
+      // if (!_.isArray(req.query.methods)) {
+      //   response['message'] = 'El campo \'methods\' debe ser un arreglo';
+      //   response['result'] = req.query;
+
+      //   return res.status(StatusCodes.BAD_REQUEST).json(response);
+      // }
+
+      for (const method of req.query.methods) {
+        if (!validator.isAlpha(method)) {
+          response['message'] = 'El campo "methods" debe ser una cadena de caracteres';
+          response['result'] = req.query;
+
+          return res.status(StatusCodes.BAD_REQUEST).json(response);
+        }
+
+        if (!_.contains(['get', 'post', 'put', 'delete'], method.toLowerCase())) {
+          response['message'] = 'El valor del campo "methods" no permitido';
+          response['result'] = req.query;
+
+          return res.status(StatusCodes.BAD_REQUEST).json(response);
+        }
+
+        methods.push(method);
+      }
+    }
+
+    const { data, isOk } = BasicService.getOperations(methods);
+
+    response['result'] = data;
+    response['isOk'] = isOk;
 
     console.log('Devolviendo respuesta...');
 
-    if (result.isOk === true) {
+    if (isOk === true) {
+      response['message'] = 'Devolviendo los datos exitosamente';
+      return res.status(StatusCodes.OK).json(response);
+    } else {
+      response['message'] = result.error;
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+/**
+ * Obtención de una operación específica
+ *
+ * Método que tiene como función devolver un dato en específico
+ *
+ * @param {Request} req - objeto de solicitud http
+ * @param {Response} res - objeto de respuesta http
+ * @returns {json} un objeto json con 3 atributos:
+ * - isOk: booleano que indica si la solicitud es exitosa
+ * - message: un mensaje descriptivo de la respuesta
+ * - result: un arreglo con el resultado de la consulta.
+ */
+const getOperation = (req, res) => {
+  try {
+    console.log('Entrando al controlador getOperation...');
+
+    const response = {};
+
+    response['isOk'] = false;
+
+    if (!_.contains(['get', 'post', 'put', 'delete'], req.params.method.toLowerCase())) {
+      response['message'] = 'El valor del campo "method" no es permitido';
+      response['result'] = req.params;
+
+      return res.status(StatusCodes.BAD_REQUEST).json(response);
+    }
+
+    const { data, isOk } = BasicService.getOperation(req.params.method);
+
+    response['result'] = data;
+    response['isOk'] = isOk;
+
+    console.log('Devolviendo respuesta...');
+
+    if (isOk === true) {
       response['message'] = 'Devolviendo los datos exitosamente';
       return res.status(StatusCodes.OK).json(response);
     } else {
@@ -90,14 +174,14 @@ const postAddition = (req, res) => {
 
   const num1 = req.body.num1;
   const num2 = req.body.num2;
-  const result = BasicService.postAddition(num1, num2);
+  const { data, isOk } = BasicService.postAddition(num1, num2);
 
-  response['result'] = result.data;
-  response['isOk'] = result.isOk;
+  response['result'] = data;
+  response['isOk'] = isOk;
 
   console.log('Devolviendo respuesta...');
 
-  if (result.isOk === true) {
+  if (isOk === true) {
     response['message'] = 'Devolviendo los datos exitosamente';
     return res.status(StatusCodes.OK).json(response);
   } else {
@@ -153,14 +237,14 @@ const putMultiply = (req, res) => {
 
   const num1 = req.body.num1;
   const num2 = req.body.num2;
-  const result = BasicService.putMultiply(num1, num2);
+  const { data, isOk } = BasicService.putMultiply(num1, num2);
 
-  response['result'] = result.data;
-  response['isOk'] = result.isOk;
+  response['result'] = data;
+  response['isOk'] = isOk;
 
   console.log('Devolviendo respuesta...');
 
-  if (result.isOk === true) {
+  if (isOk === true) {
     response['message'] = 'Devolviendo los datos exitosamente';
     return res.status(StatusCodes.OK).json(response);
   } else {
@@ -223,14 +307,14 @@ const deleteDivision = (req, res) => {
 
   const num1 = req.body.num1;
   const num2 = req.body.num2;
-  const result = BasicService.deleteDivision(num1, num2);
+  const { data, isOk } = BasicService.deleteDivision(num1, num2);
 
-  response['result'] = result.data;
-  response['isOk'] = result.isOk;
+  response['result'] = data;
+  response['isOk'] = isOk;
 
   console.log('Devolviendo respuesta...');
 
-  if (result.isOk === true) {
+  if (isOk === true) {
     response['message'] = 'Devolviendo los datos exitosamente';
     return res.status(StatusCodes.OK).json(response);
   } else {
@@ -248,12 +332,13 @@ const deleteDivision = (req, res) => {
  */
 const getFrontpage = (req, res) => {
   return res.sendFile(path.join(__dirname, '../../views/index.html'));
-}
+};
 
 module.exports = {
   getOperations,
+  getOperation,
   postAddition,
   putMultiply,
   deleteDivision,
-  getFrontpage
+  getFrontpage,
 };
