3d8c43b3ae42ec411b0117d93431e18692216856
Strangexz <ja_figueroa@outlook.es>
2024-06-18 18:26:49 -0600

    feat(): nueva ruta

--- src/api/routes/basic.route.js
+++ src/api/routes/basic.route.js
@@ -4,7 +4,8 @@ const BasicController = require('../controllers/basic.controller');
 const basic = express.Router();
 
 // Definición de rutas
-basic.get('/basic/getOperations', BasicController.getOperations);
+basic.get('/basic/getAllOperations', BasicController.getOperations);
+basic.get('/basic/getOneOperation/:method', BasicController.getOperation);
 basic.post('/basic/postAddition', BasicController.postAddition);
 basic.put('/basic/putMultiply', BasicController.putMultiply);
 basic.delete('/basic/deleteDivision', BasicController.deleteDivision);
