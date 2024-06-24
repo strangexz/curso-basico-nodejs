0eb1605cc9576d4fcc629bbe14b9e437eb149894
Strangexz <ja_figueroa@outlook.es>
2024-06-24 16:48:15 -0600

    config(): agregados archivos de carga de base de datos

---
+++ database/seeds/04_pokemon_types.js
@@ -0,0 +1,12 @@
+const tableName = 'pokemon_types';
+
+const data = require('../data/dev/pokemon_types.json');
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex(tableName).del();
+  await knex(tableName).insert(data);
+};
---
+++ database/seeds/07_trainers.js
@@ -0,0 +1,12 @@
+const tableName = 'trainers';
+
+const data = require('../data/dev/trainers.json');
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex(tableName).del();
+  await knex(tableName).insert(data);
+};
