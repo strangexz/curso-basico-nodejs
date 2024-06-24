6df700dfa7411bdf3ae95ce4f35d73ddeaaafa23
Strangexz <ja_figueroa@outlook.es>
2024-06-24 16:44:40 -0600

    config(): agregado archivo de configuración de knex

--- knexfile.js
+++ knexfile.js
@@ -7,9 +7,17 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './dev.sqlite3'
-    }
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: __dirname + '/database/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/database/seeds',
+    },
   },
 
   staging: {
