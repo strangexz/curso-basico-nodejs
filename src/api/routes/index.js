d2f3201c9589976b83be72acbf4a217b8eabcff9
Strangexz <ja_figueroa@outlook.es>
2024-06-28 17:49:04 -0600

    feat(): agregadas las rutas de tipos

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const basicRoutes = require('./basic.route');
 const attackTypesRoutes = require('./attackTypes.route');
+const typesRoutes = require('./type.route');
 
 const api = express();
 
 api.use(basicRoutes);
 api.use(attackTypesRoutes);
+api.use(typesRoutes);
 
 module.exports = api;
