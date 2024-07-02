773342417feb669eddb58f14e6993c42e0e698aa
Strangexz <ja_figueroa@outlook.es>
2024-07-01 18:58:20 -0600

    feat(): agregado el servicio que devuelve un tipo de pokemon

--- src/api/services/type.service.js
+++ src/api/services/type.service.js
@@ -19,9 +19,9 @@ const getTypesService = async () => {
     log.verbose('Inicio servicio getTypesService');
 
     const data = await Type.query()
-    // .select('id', 'name', 'id_attack_type', 'created_at', 'updated_at')
-    .where('is_deleted', false)
-    .withGraphFetched('attacktype');
+      .select('id', 'name', 'id_attack_type', 'created_at', 'updated_at')
+      .where('is_deleted', false)
+      .withGraphFetched('attackTypes(defaultSelects)');
 
     log.debug(JSON.stringify(data));
     log.verbose('Fin servicio getTypesService');
@@ -34,6 +34,40 @@ const getTypesService = async () => {
   }
 };
 
+/**
+ * Este servicio consulta a la base de datos de un tipo tipos de pokemon en específico
+ * por su identificador
+ *
+ * @returns un objeto json con 3 atributos:
+ * - isOk: valor booleano si es 'true' indica que hubo un error
+ *   si es 'false' no hubo error alguno.
+ * - data: un arreglo con el resultado de la consulta/transacción
+ *   realizada, si ocurrió un error se enviará un valor nulo.
+ * - error: en caso de haber un error aquí irá la descripción
+ *   de lo contrario se enviará un valor nulo.
+ */
+const getTypeService = async (id) => {
+  try {
+    log.verbose('Inicio servicio getTypeService');
+
+    const data = await Type.query()
+      .select('id', 'name', 'id_attack_type', 'created_at', 'updated_at')
+      .findById(id)
+      .where('is_deleted', false)
+      .withGraphFetched('attackTypes(defaultSelects)');
+
+    log.debug(JSON.stringify(data));
+    log.verbose('Fin servicio getTypeService');
+
+    return { isOk: true, data, error: null };
+  } catch (e) {
+    console.error(e.stack);
+    log.error(e.message);
+    return { isOk: false, data: null, error: e.message };
+  }
+}
+
 module.exports = {
   getTypesService,
+  getTypeService
 };
